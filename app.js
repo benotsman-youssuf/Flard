ff4d54430e6ca6f367310a39a9f4f70f9bfb2708
youssef <benotsman.youssuf@gmail.com>
2024-04-25 02:24:23 +0100

    add dialog popup

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('.buttonSec');
+const dialog = document.querySelector('.popup');
 
 button.addEventListener('click', () => {
-  const card = document.createElement('div');
-  card.classList.add('card');
-  container.appendChild(card);
+    const card = document.createElement('button');
+    card.classList.add('card');
+    container.appendChild(card);
+
+    card.addEventListener('click', () => {
+        const popup = document.createElement('dialog');
+        const close = document.createElement('button');
+        close.textContent = 'X';
+
+        popup.appendChild(close);
+        card.appendChild(popup);
+
+        close.classList.add('close');
+        popup.classList.add('popup');
+        popup.showModal();
+
+        close.addEventListener('click', () => {
+            popup.close(); // Close the dialog when the close button is clicked
+        });
+    });
 });
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
