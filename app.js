8e790b0e55998faddac149c0f081a4ac5056e6fd
youssef <benotsman.youssuf@gmail.com>
2024-04-26 17:46:04 +0100

    add container and addin card button to dialog

--- app.js
+++ app.js
@@ -10,18 +10,27 @@ button.addEventListener('click', () => {
     card.addEventListener('click', () => {
         const popup = document.createElement('dialog');
         const close = document.createElement('button');
+        const AddFlashcard = document.createElement('button');
+        const flashcardContainer = document.createElement('div');
+
+        AddFlashcard.innerHTML = '<i class="fa-solid fa-cards-blank fa-2xl"></i>';
         close.textContent = 'X';
 
+        popup.appendChild(flashcardContainer);
+        popup.appendChild(AddFlashcard);
         popup.appendChild(close);
         card.appendChild(popup);
 
+        flashcardContainer.classList.add('flashcardContainer');
         close.classList.add('close');
         popup.classList.add('popup');
+        AddFlashcard.classList.add('AddFlashcard');
         popup.showModal();
 
         close.addEventListener('click', () => {
             popup.close(); // Close the dialog when the close button is clicked
         });
+
     });
 });
 
