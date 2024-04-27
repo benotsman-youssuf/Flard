ae7e60ecd3232ce1446b4fbe51719f391789a5c8
youssef <benotsman.youssuf@gmail.com>
2024-04-27 11:58:26 +0100

    add flash crds with back and front without animation

--- app.js
+++ app.js
@@ -25,10 +25,35 @@ button.addEventListener('click', () => {
         close.classList.add('close');
         popup.classList.add('popup');
         AddFlashcard.classList.add('AddFlashcard');
-        popup.showModal();
 
+        popup.showModal(); // Show the dialog
         close.addEventListener('click', () => {
             popup.close(); // Close the dialog when the close button is clicked
+        });
+        AddFlashcard.addEventListener('click' , () => {
+            const flash_card = document.createElement('div');
+            const front = document.createElement('div');
+            const back = document.createElement('div');
+            const flip = document.createElement('button');
+
+            flash_card.classList.add('flash_card');
+            front.classList.add('front');
+            back.classList.add('back');
+            flip.classList.add('flip');
+
+            flash_card.appendChild(front);
+            flash_card.appendChild(back);
+            flash_card.appendChild(flip);
+            flashcardContainer.appendChild(flash_card);
+
+            flip.textContent = 'Flip';
+            front.textContent = 'Front';
+            back.textContent = 'Back';
+
+            flip.addEventListener('click', () => {
+                flash_card.style.transform = '    transform: rotateY(180deg)';
+                
+            });
         });
 
     });
